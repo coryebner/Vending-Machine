9606b75bc205254afd5627165e26fba0412466cb
Christopher Smith <soccer_is_really_cool@yahoo.ca>
2015-04-02 00:52:28 -0600

    initial commit

---
+++ integration/VendingMachine/src/business/notifications/InternetConnectionLightController.java
@@ -0,0 +1,20 @@
+package notifications;
+
+import hardware.ui.IndicatorLight;
+
+public class InternetConnectionLightController {
+
+	IndicatorLight light;
+	public InternetConnectionLightController(IndicatorLight light) {
+		this.light = light;
+	}
+	
+	//Listen for Internet turn ON
+			//turn on on
+	
+	//Listen for Internet turn OFF
+			//turn on off
+
+	
+	
+}
