c4a57819b20df2cc5d6a2274f362ac9df82b8cb9
dominichul <dahul@ucalgary.ca>
2015-04-03 17:39:11 -0600

    added ConfigurationPanelLogic to business.config

---
+++ integration/VendingMachine/src/business/config/ConfigPanelLogic.java
@@ -0,0 +1,147 @@
+package business.config;
+
+import hardware.AbstractHardware;
+import hardware.AbstractHardwareListener;
+import hardware.ui.ConfigurationPanelTransmitter;
+
+import java.util.Scanner;
+
+import business.selection_delivery.AbstractController;
+
+public class ConfigPanelLogic extends AbstractController<ConfigurationListener> implements hardware.ui.ConfigurationPanelTransmitterListener {
+
+	static int state;
+	static int secondaryState;
+	
+	int productId;
+	int newPrice;
+	String newName;
+	
+	String tempProdId;
+	String tempPrice;
+	String tempName;
+	
+	public ConfigPanelLogic(){
+		state = 0;
+		secondaryState = 0;
+		tempProdId = "";
+		tempPrice = "";
+		tempName = "";
+	}
+	
+	public void commandEntered(ConfigurationPanelTransmitter transmitter, String keyCode){
+		switch(state){
+		case 0:
+			if(keyCode.equals("newPrice"))
+				state = 1;
+			else if(keyCode.equals("newName"))
+				state = 2;
+			break;
+		case 1:
+			switch(secondaryState){
+			case 0:
+				System.out.println(tempProdId + "dd");
+				if(isNumeric(keyCode)){
+					tempProdId = tempProdId + keyCode;
+				}
+				else if(keyCode.equals("ENTER")){
+					productId = Integer.parseInt(tempProdId);
+					tempProdId = "";
+					secondaryState = 1;
+				}
+				break;
+			case 1:
+				if(isNumeric(keyCode)){
+					tempPrice = tempPrice + keyCode;
+				}
+				else if(keyCode.equals("ENTER")){
+					System.out.println("Enter pressed");
+					newPrice = Integer.parseInt(tempPrice);
+					tempPrice = "";
+					secondaryState = 0;
+					state = 0;
+					System.out.println("Notifying Listeners: " + productId + "  " + newPrice);
+					//notify hardware through listener
+					//transmitter.enterCommand("newPrice" + " " + productId + " " + newPrice);  Not sure what to send back
+					//																			to hardware or how they want the display message
+					//																			pushed back to them
+					notifyNameChange();
+				}
+				break;
+			}
+			break;
+			
+		case 2:
+			switch(secondaryState){
+			case 0:
+				if(isNumeric(keyCode)){
+					tempProdId = tempProdId + keyCode;
+				}
+				else if(keyCode.equals("ENTER")){
+					productId = Integer.parseInt(tempProdId);
+					tempProdId = "";
+					secondaryState = 1;
+				}
+				break;
+			case 1:
+				if(keyCode.length() == 1){
+					tempName = tempName + keyCode;
+				}
+				else if(keyCode.equals("ENTER")){
+					newName = tempName;
+					tempName = "";
+					secondaryState = 0;
+					state = 0;
+					System.out.println("Notifying Listeners: " + productId + "  " + newName);
+					//notify hardware through listener
+					//transmitter.enterCommand("newName" + " " + productId + " " + newPrice); Same as the comment above
+					notifyNameChange();
+				}
+				break;
+			}
+			break;
+		}
+	}
+	
+	
+	public static boolean isNumeric(String str)  
+	{  
+	  try  
+	  {  
+	    Integer.parseInt(str);  
+	  }  
+	  catch(NumberFormatException nfe)  
+	  {  
+	    return false;  
+	  }  
+	  return true;  
+	}
+	
+	protected void notifyPriceChange()
+	{
+		Class<?>[] parameterTypes =
+		        new Class<?>[] { };
+		Object[] args = new Object[] { this,productId,newPrice };
+		notifyListeners(ConfigurationListener.class, "priceChanged", parameterTypes, args);
+	}
+	protected void notifyNameChange()
+	{
+		Class<?>[] parameterTypes =
+		        new Class<?>[] { };
+		Object[] args = new Object[] { this,productId,newName };
+		notifyListeners(ConfigurationListener.class, "nameChanged", parameterTypes, args);
+	}
+
+	@Override
+	public void enabled(AbstractHardware<AbstractHardwareListener> hardware) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void disabled(AbstractHardware<AbstractHardwareListener> hardware) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
