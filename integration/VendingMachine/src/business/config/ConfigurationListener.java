7cfd2b3c362d9034ac78084d1051bb35bf68900b
dominichul <dahul@ucalgary.ca>
2015-04-03 18:15:32 -0600

    edit to ConfigurationListener

--- integration/VendingMachine/src/business/config/ConfigurationListener.java
+++ integration/VendingMachine/src/business/config/ConfigurationListener.java
@@ -1,6 +1,8 @@
 package business.config;
 
-public interface ConfigurationListener {
+import business.selection_delivery.AbstractControllerListener;
+
+public interface ConfigurationListener extends AbstractControllerListener {
 	public void nameChanged(int index, String newName);
 	public void priceChanged(int index, int newPrice);
 }
