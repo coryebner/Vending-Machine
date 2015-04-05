fe6784ee5c471b9f06f1115c3799d0b72fa9499d
Byron Seto <byronseto@hotmail.com>
2015-04-04 20:36:05 -0600

    Created the test file

--- integration/VendingMachine/src/business/touchscreen/tests/BusinessTouchScreenTest.java
+++ integration/VendingMachine/src/business/touchscreen/tests/BusinessTouchScreenTest.java
@@ -1,5 +1,69 @@
 package business.touchscreen.tests;
 
+import static org.junit.Assert.*;
+
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
+import java.awt.event.KeyEvent;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class BusinessTouchScreenTest {
 
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	public static void main(String[] args) {
+        KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyEventDispatcher() {
+
+            @Override
+            public boolean dispatchKeyEvent(KeyEvent ke) {
+                synchronized (IsKeyPressed.class) {
+                    switch (ke.getID()) {
+                    case KeyEvent.KEY_PRESSED:
+                        if (ke.getKeyCode() == KeyEvent.VK_W) {
+                            wPressed = true;
+                        }
+                        break;
+
+                    case KeyEvent.KEY_RELEASED:
+                        if (ke.getKeyCode() == KeyEvent.VK_W) {
+                            wPressed = false;
+                        }
+                        break;
+                    }
+                    return false;
+                }
+            }
+
+			@Override
+			public boolean dispatchKeyEvent(KeyEvent arg0) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+        });
+	
+	
 }
