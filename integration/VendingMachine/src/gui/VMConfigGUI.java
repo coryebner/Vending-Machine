4b752c53f496f8f06b63ed29cbb0bfeb87d29bd9
eriegz <evanriegel@Evan-Riegels-MacBook-Pro.local>
2015-04-06 17:46:33 -0600

    Removing the options for the 2 configurations which were removed from scope (the ones with no touch screen which required PayPal).

--- integration/VendingMachine/src/gui/VMConfigGUI.java
+++ integration/VendingMachine/src/gui/VMConfigGUI.java
@@ -53,7 +53,7 @@ public class VMConfigGUI {
 	private void initialize() {
 		frmVendingMachinesRUS = new JFrame();
 		frmVendingMachinesRUS.setTitle("Vending Machines R Us");
-		frmVendingMachinesRUS.setBounds(100, 100, 386, 500);
+		frmVendingMachinesRUS.setBounds(100, 100, 386, 440);
 		frmVendingMachinesRUS.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel panel = new JPanel();
@@ -75,53 +75,54 @@ public class VMConfigGUI {
 		//Auto-generated layout spaghetti code
 		GroupLayout groupLayout = new GroupLayout(frmVendingMachinesRUS.getContentPane());
 		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
+			groupLayout.createParallelGroup(Alignment.TRAILING)
 				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 177, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(panel_4, GroupLayout.DEFAULT_SIZE, 190, Short.MAX_VALUE)
+						.addComponent(panel_2, GroupLayout.DEFAULT_SIZE, 190, Short.MAX_VALUE)
+						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+							.addComponent(lblConfigFile)
+							.addGap(62)))
+					.addContainerGap())
+				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(panel, GroupLayout.DEFAULT_SIZE, 374, Short.MAX_VALUE)
+					.addContainerGap())
+				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
 					.addGap(22)
 					.addComponent(lblVendingMachineType)
 					.addPreferredGap(ComponentPlacement.RELATED, 39, Short.MAX_VALUE)
 					.addComponent(lblRemoteReportingType)
 					.addGap(30))
-				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
 					.addContainerGap()
-					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-						.addComponent(panel_3, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 374, Short.MAX_VALUE)
-						.addComponent(panel, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 374, Short.MAX_VALUE)
-						.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
-							.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 177, GroupLayout.PREFERRED_SIZE)
-							.addGap(7)
-							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-								.addGroup(groupLayout.createSequentialGroup()
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-										.addComponent(panel_2, GroupLayout.DEFAULT_SIZE, 190, Short.MAX_VALUE)
-										.addComponent(panel_4, GroupLayout.DEFAULT_SIZE, 190, Short.MAX_VALUE)))
-								.addGroup(groupLayout.createSequentialGroup()
-									.addGap(60)
-									.addComponent(lblConfigFile)))))
+					.addComponent(panel_3, GroupLayout.DEFAULT_SIZE, 374, Short.MAX_VALUE)
 					.addContainerGap())
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(16)
+					.addContainerGap()
 					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblVendingMachineType)
 						.addComponent(lblRemoteReportingType))
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
 						.addGroup(groupLayout.createSequentialGroup()
 							.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 86, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED, 29, Short.MAX_VALUE)
+							.addPreferredGap(ComponentPlacement.RELATED)
 							.addComponent(lblConfigFile)
 							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(panel_4, GroupLayout.PREFERRED_SIZE, 166, GroupLayout.PREFERRED_SIZE))
-						.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 303, Short.MAX_VALUE))
+							.addComponent(panel_4, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+						.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 258, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(panel_3, GroupLayout.PREFERRED_SIZE, 77, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap())
+					.addContainerGap(67, Short.MAX_VALUE))
 		);
 		panel_3.setLayout(null);
 
@@ -149,74 +150,74 @@ public class VMConfigGUI {
 		panel_1.add(machineConfig3);
 		machineConfig3.addActionListener(VMConfigLisenter);
 		
-		JRadioButton machineConfig4 = new JRadioButton("VMRUS-COM-P/MI");
-		machineConfig4.setActionCommand("VMRUS-COM-P/MI");
+//		JRadioButton machineConfig4 = new JRadioButton("VMRUS-COM-P/MI");
+//		machineConfig4.setActionCommand("VMRUS-COM-P/MI");
+//		machineConfig4.setBounds(6, 78, 172, 23);
+//		panel_1.add(machineConfig4);
+//		machineConfig4.addActionListener(VMConfigLisenter);
+		
+		JRadioButton machineConfig4 = new JRadioButton("VMRUS-COM-P/M");
+		machineConfig4.setActionCommand("VMRUS-COM-P/M");
 		machineConfig4.setBounds(6, 78, 172, 23);
 		panel_1.add(machineConfig4);
 		machineConfig4.addActionListener(VMConfigLisenter);
 		
-		JRadioButton machineConfig5 = new JRadioButton("VMRUS-COM-P/M");
-		machineConfig5.setActionCommand("VMRUS-COM-P/M");
+		JRadioButton machineConfig5 = new JRadioButton("VMRUS-TOC-P/MI");
+		machineConfig5.setActionCommand("VMRUS-TOC-P/MI");
 		machineConfig5.setBounds(6, 102, 172, 23);
 		panel_1.add(machineConfig5);
 		machineConfig5.addActionListener(VMConfigLisenter);
 		
-		JRadioButton machineConfig6 = new JRadioButton("VMRUS-TOC-P/MI");
-		machineConfig6.setActionCommand("VMRUS-TOC-P/MI");
+		JRadioButton machineConfig6 = new JRadioButton("VMRUS-TOC-P/I");
+		machineConfig6.setActionCommand("VMRUS-TOC-P/I");
 		machineConfig6.setBounds(6, 126, 172, 23);
 		panel_1.add(machineConfig6);
 		machineConfig6.addActionListener(VMConfigLisenter);
 		
-		JRadioButton machineConfig7 = new JRadioButton("VMRUS-TOC-P/I");
-		machineConfig7.setActionCommand("VMRUS-TOC-P/I");
+//		JRadioButton machineConfig8 = new JRadioButton("VMRUS-COM-C/MI");
+//		machineConfig8.setActionCommand("VMRUS-COM-C/MI");
+//		machineConfig8.setBounds(6, 174, 172, 23);
+//		panel_1.add(machineConfig8);
+//		machineConfig8.addActionListener(VMConfigLisenter);
+		
+		JRadioButton machineConfig7 = new JRadioButton("VMRUS-COM-C/M");
+		machineConfig7.setActionCommand("VMRUS-COM-C/M");
 		machineConfig7.setBounds(6, 150, 172, 23);
 		panel_1.add(machineConfig7);
 		machineConfig7.addActionListener(VMConfigLisenter);
 		
-		JRadioButton machineConfig8 = new JRadioButton("VMRUS-COM-C/MI");
-		machineConfig8.setActionCommand("VMRUS-COM-C/MI");
+		JRadioButton machineConfig8 = new JRadioButton("VMRUS-TOC-C/MI");
+		machineConfig8.setActionCommand("VMRUS-TOC-C/MI");
 		machineConfig8.setBounds(6, 174, 172, 23);
 		panel_1.add(machineConfig8);
 		machineConfig8.addActionListener(VMConfigLisenter);
 		
-		JRadioButton machineConfig9 = new JRadioButton("VMRUS-COM-C/M");
-		machineConfig9.setActionCommand("VMRUS-COM-C/M");
+		JRadioButton machineConfig9 = new JRadioButton("VMRUS-TOC-C+");
+		machineConfig9.setActionCommand("VMRUS-TOC-C+");
 		machineConfig9.setBounds(6, 198, 172, 23);
 		panel_1.add(machineConfig9);
 		machineConfig9.addActionListener(VMConfigLisenter);
 		
-		JRadioButton machineConfig10 = new JRadioButton("VMRUS-TOC-C/MI");
-		machineConfig10.setActionCommand("VMRUS-TOC-C/MI");
+		JRadioButton machineConfig10 = new JRadioButton("VMRUS-TOC-C+/I");
+		machineConfig10.setActionCommand("VMRUS-TOC-C+/I");
 		machineConfig10.setBounds(6, 222, 172, 23);
 		panel_1.add(machineConfig10);
-		machineConfig10.addActionListener(VMConfigLisenter);
-		
-		JRadioButton machineConfig11 = new JRadioButton("VMRUS-TOC-C+");
-		machineConfig11.setActionCommand("VMRUS-TOC-C+");
-		machineConfig11.setBounds(6, 246, 172, 23);
-		panel_1.add(machineConfig11);
-		machineConfig11.addActionListener(VMConfigLisenter);
-		
-		JRadioButton machineConfig12 = new JRadioButton("VMRUS-TOC-C+/I");
-		machineConfig12.setActionCommand("VMRUS-TOC-C+/I");
-		machineConfig12.setBounds(6, 270, 172, 23);
-		panel_1.add(machineConfig12);
 		panel.setLayout(null);
-		machineConfig12.addActionListener(VMConfigLisenter);
+		machineConfig10.addActionListener(VMConfigLisenter);
 		
 		machineConfigRadioGroup = new ButtonGroup();
 		machineConfigRadioGroup.add(machineConfig1);
 		machineConfigRadioGroup.add(machineConfig2);
 		machineConfigRadioGroup.add(machineConfig3);
+//		machineConfigRadioGroup.add(machineConfig4);
 		machineConfigRadioGroup.add(machineConfig4);
 		machineConfigRadioGroup.add(machineConfig5);
 		machineConfigRadioGroup.add(machineConfig6);
+//		machineConfigRadioGroup.add(machineConfig8);
 		machineConfigRadioGroup.add(machineConfig7);
 		machineConfigRadioGroup.add(machineConfig8);
 		machineConfigRadioGroup.add(machineConfig9);
 		machineConfigRadioGroup.add(machineConfig10);
-		machineConfigRadioGroup.add(machineConfig11);
-		machineConfigRadioGroup.add(machineConfig12);
 		
 		ActionListener reportConfigListener = new ReportConfigListener();
 		//Remote reporting configurations
@@ -247,7 +248,7 @@ public class VMConfigGUI {
 		//Config file
 		JLabel lblwhenYouClick = new JLabel("<html>When you click this button,<br>the program launches a<br>vending machine based on<br>the settings stored in its<br>.config file.</html>");
 		lblwhenYouClick.setForeground(Color.DARK_GRAY);
-		lblwhenYouClick.setBounds(6, 6, 178, 83);
+		lblwhenYouClick.setBounds(6, 10, 178, 83);
 		panel_4.add(lblwhenYouClick);
 		
 		JButton btnConfigFile = new JButton("Use Config File");
@@ -256,7 +257,7 @@ public class VMConfigGUI {
 				
 			}
 		});
-		btnConfigFile.setBounds(24, 114, 139, 29);
+		btnConfigFile.setBounds(26, 104, 139, 29);
 		panel_4.add(btnConfigFile);
 		panel_2.setLayout(null);
 		
@@ -264,20 +265,18 @@ public class VMConfigGUI {
 		JButton btnLaunch = new JButton("Launch");
 		btnLaunch.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) { 
-				/* Gets vending machine from other place and send it as an argument to the GUI
-				if(reportConfig!=null && machineConfig!=null){	
-					AbstractVendingMachine machine = loadVendingMachine();
-					if(machine.getTouchScreen != null){
-						Launch the touchscreen GUI
-					
-					}else{
-						GUI window = new GUI(machine, parts);
-						window.frmVendingMachines.setVisible(true);
-						frmVendingMachinesRUS.setVisible(false);	
-					}
-				 }
-				 */
-				
+//				if(reportConfig!=null && machineConfig!=null){	
+//					AbstractVendingMachine machine = loadVendingMachine();
+//					if(machine.getTouchScreen != null){
+//						Launch the touchscreen GUI
+//					
+//					}else{
+//						GUI window = new GUI(machine, parts);
+//						window.frmVendingMachines.setVisible(true);
+//						frmVendingMachinesRUS.setVisible(false);	
+//					}
+//				 }
+
 				// Vending Machine # 1
 				if(machineConfig.equals("VMRUS-SFF-P/C") && reportConfig!=null){
 					//Here use the two selected configurations to create the Vending Machine	
