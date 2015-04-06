bcb09e42bf135c66930ed2cf774faed6292f0043
Christopher Smith <soccer_is_really_cool@yahoo.ca>
2015-04-06 15:57:51 -0600

    registerd to return coin events

--- integration/VendingMachine/src/business/notifications/CoinsAvailableLightController.java
+++ integration/VendingMachine/src/business/notifications/CoinsAvailableLightController.java
@@ -1,4 +1,4 @@
-package notifications;
+package business.notifications;
 
 import hardware.AbstractHardware;
 import hardware.AbstractHardwareListener;
@@ -10,73 +10,23 @@ import hardware.ui.IndicatorLight;
 import java.util.Iterator;
 import java.util.Vector;
 
+import business.funds.ExactChangeController;
 
-/*
- * two possible implementations
- * 
- * 1) CoinsAvailable listens for funds to tell us when to turn the light on
- * 			-MUST MUST MUST assume that coins are providing us with a change light event frequently enough
- * 2) We listen ourselves to figure out when to turn on the light.
- */
-
-public class CoinsAvailableLightController implements CoinRackListener{
-
-	//METHOD 1 -----------------------------------
+public class CoinsAvailableLightController implements ExactChangeControllerListener{
 	
-	
-	//Funds tells us to turn on light
-			//turn on light
-	
-	//Funds tells us to turn off light
-			//turn off light
-	
-	//METHOD 2 -----------------------------------
-	
-	int[] rackDenominations;
-	Vector<Integer> changeToMake;
 	IndicatorLight light;
-	InternalCoinsAvailable internalCoinsAvailable;
 	
-	public CoinsAvailableLightController(IndicatorLight light, CoinRack racks[], coinRackListener coinRack, int[] rackDenominations, int[] popPrices) {
-		for (int i = 0; i < racks.length; i++) {
-			racks[i].register(this);
-			
-		}
-		changeToMake = new Vector<Integer>();
-		this.internalCoinsAvailable = ica;
+	public CoinsAvailableLightController(IndicatorLight light, ExactChangeController ecc) {
 		this.light = light;
-		this.rackDenominations = rackDenominations;
-
-		determineChangeToMake(rackDenominations, popPrices);
-		refreshLightState();
+		ecc.register(this);
 	}
-
-	@Override
-	public void enabled(AbstractHardware<AbstractHardwareListener> hardware) {		
-	}
-
-	@Override
-	public void disabled(AbstractHardware<AbstractHardwareListener> hardware) {		
-	}
-
-	@Override
-	public void coinsFull(CoinRack rack) {		
-	}
-
-	@Override
-	public void coinsEmpty(CoinRack rack) {		
-	}
-
-	@Override
-	public void coinAdded(CoinRack rack, Coin coin) {		
-	}
-
-	@Override
-	public void coinRemoved(CoinRack rack, Coin coin) {		
-	}
-
 	
 	
+	public void exactChangeAvailable(ExactChangeController ecc) {
+		light.deactivate();
+	}
 	
-	
+	public void exactChangeUnavailable(ExactChangeController ecc) {
+		light.activate();
+	}
 }
