920e52180ffe9469b27bceb55c5b65ab4580dc62
William Wright <wwright@ucalgary.ca>
2015-04-10 12:14:54 -0600

    updated hardware-Fund interface to reflect recent removal of locale field in AbstractFund

--- integration/VendingMachine/src/hardware/funds/IFund.java
+++ integration/VendingMachine/src/hardware/funds/IFund.java
@@ -2,9 +2,6 @@ package hardware.funds;
 
 import hardware.products.IRackable;
 
-import java.util.Locale;
-
 public interface IFund extends IRackable {
 	public int getValue();
-	public Locale getLocale();
 }
