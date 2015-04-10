637848705e684c0f529d7fc5adec04d678d146aa
William Wright <wwright@ucalgary.ca>
2015-04-10 12:13:52 -0600

    removed unnecessary locale field for hardware specific AbstractFunds class as this field/data is handled by business.funds and based off the machine locale field.

--- integration/VendingMachine/src/hardware/funds/AbstractFund.java
+++ integration/VendingMachine/src/hardware/funds/AbstractFund.java
@@ -1,12 +1,9 @@
 package hardware.funds;
 
-import java.util.Locale;
-
 import hardware.exceptions.SimulationException;
 
 public abstract class AbstractFund implements IFund {
     private final int value;
-    private final Locale locale;
     
     /**
      * Creates an fund with the specified value in cents.
@@ -20,9 +17,6 @@ public abstract class AbstractFund implements IFund {
 	if(value <= 0)
 	    throw new SimulationException("The value of the " + this.getClass() + " must be positive: " + value);
 	this.value = value;
-	
-	/* Default to Canadian Locale */
-	this.locale = Locale.CANADA;
     }
     
     /**
@@ -30,29 +24,6 @@ public abstract class AbstractFund implements IFund {
      */
     public int getValue() {
     	return this.value;
-    }
-    
-    /** 
-     * Returns the locale of the Fund
-     */
-	public Locale getLocale() {
-		return this.locale;
-	}
-
-	/**
-	 * Comparison of one fund to another first compares value in cents and then locale.
-	 * 
-	 * @param fund
-	 * 		   the fund to be compared
-	 * @return
-	 * 		   true if both value in cents and locale of the fund parameter match the current fund object
-	 */
-	public int compareTo(IFund fund) {
-		int ret = Integer.compare(this.getValue(), fund.getValue());
-		if (ret == 0) {
-			return this.locale.equals(fund.getLocale()) ? 0 : ret;
-		}
-		return ret;
-	}
+    } 
      
 }
